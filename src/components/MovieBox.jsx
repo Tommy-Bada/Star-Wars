f90f09834b594c2d3d5598c5c481fc7cd576b9c0
Tomiwa Bada <tomiwabd@gmail.com>
2023-10-09 00:12:53 +0100

    Border radius added

--- src/components/MovieBox.jsx
+++ src/components/MovieBox.jsx
@@ -3,7 +3,7 @@
 function MovieBox({ title, releaseDate, openCrawl, handleMouseOver }) {
   return (
     <div
-      className="movie-box w-[100%] p-[2rem] my-[2rem] bg-gray-500 bg-boxBg  bg-blend-multiply sm:min-w-[48%] sm:max-w-[48%] lg:min-w-[32%] lg:max-w-[32%] hover:shadow-[0px_0px_6px_4px_rgba(111,151,222,1)]"
+      className="movie-box w-[100%] p-[2rem] my-[2rem] bg-gray-500 bg-boxBg  bg-blend-multiply sm:min-w-[48%] sm:max-w-[48%] lg:min-w-[32%] lg:max-w-[32%] hover:shadow-[0px_0px_6px_4px_rgba(111,151,222,1)] rounded-md"
       onClick={handleMouseOver}
     >
       <h3 className="text-[2rem]">{title}</h3>
